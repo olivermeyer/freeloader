099e11b3109ada23ab72e0595e9aa4fbaeb9650e
Oliver Meyer <oliver.meyer@plana.earth>
2025-09-05 11:03:23 +0200

    gitignore

--- src/demo.py
+++
@@ -1,7 +0,0 @@
-from src import soundcloud
-
-if __name__ == "__main__":
-    soundcloud.download(
-        "https://soundcloud.com/rainbowdiscoclub/rdc-093-sound-metaphors-djs",
-        "/tmp/test.mp3",
-    )
